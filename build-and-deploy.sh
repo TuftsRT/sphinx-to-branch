e337fb7676b424e6f3a0c416a361b2169d6a2e52
Uku-Kaspar Uustalu <ukukaspar.uustalu@gmail.com>
2024-09-16 12:00:38 -0400

    pull before attempting branch switch

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 home=$(pwd)
 cd $REPO_PATH
+git pull --all
 git checkout $BRANCH -- || git switch --orphan $BRANCH
 if [ "$CLEAR" == "true" ]; then git rm -rfq *; fi
 $BUILD_CMD $BUILD_ARGS $home/$SRC_DIR $OUT_DIR
