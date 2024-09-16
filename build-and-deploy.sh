c1f8ca84696148762fe8873c80c485c681f9f5d3
Uku-Kaspar Uustalu <ukukaspar.uustalu@gmail.com>
2024-09-16 11:00:47 -0400

    fix build command

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -5,7 +5,7 @@ gh repo clone $REPOSITORY $tmp
 cd $tmp
 git checkout $BRANCH -- || git switch --orphan $BRANCH
 if [ "$CLEAR" == "true" ]; then git rm -rfq *; fi
-$BUILD_CMD $BUILD_ARGS ../$SRC_DIR ./$OUT_DIR
+$BUILD_CMD $BUILD_ARGS $home/$SRC_DIR $OUT_DIR
 touch .nojekyll
 git add -A
 git commit -m "$MESSAGE"
