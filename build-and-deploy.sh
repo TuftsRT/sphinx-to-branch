d01e8554ebcf7c31388457925e7f516f7df8ec85
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2025-02-09 18:17:19 -0500

    only fail when build fails (ignore git errors)

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
 home=$(pwd)
 cd $REPO_PATH
 git pull --all
 git checkout $BRANCH -- || git switch --orphan $BRANCH
 if [ "$CLEAR" == "true" ]; then git rm -rfq *; fi
+set -e
 $BUILD_CMD $BUILD_ARGS "$home/$SRC_DIR" "./$OUT_DIR"
+set +e
 touch .nojekyll
 git add -A
 git commit -m "$MESSAGE"
