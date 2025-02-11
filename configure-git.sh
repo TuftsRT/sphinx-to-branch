c97e7e7967e079abc3503cac0761269025771a5b
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2025-02-11 10:02:58 -0500

    add option to use github-action[bot] as committer

--- configure-git.sh
+++ configure-git.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-home=$(pwd)
-cd $REPO_PATH
-git config --global user.name "$(git show -s --format='%an' HEAD)"
-git config --global user.email "$(git show -s --format='%ae' HEAD)"
-cd $home
+if [ "$USE_BOT" == "true" ]; then
+    git config --global user.name "github-actions[bot]"
+    git config --global user.email "github-actions[bot]@users.noreply.github.com"
+else
+    home=$(pwd)
+    cd $REPO_PATH
+    git config --global user.name "$(git show -s --format='%an' HEAD)"
+    git config --global user.email "$(git show -s --format='%ae' HEAD)"
+    cd $home
+fi
